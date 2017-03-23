992f308d591756584e4d63e20915e11e40fd4ef8
James Azam <azam.j.mba@aims-senegal.org>
2017-03-23 18:23:07 +0200

    practice nested data

--- Scripts/purrr.R
+++ Scripts/purrr.R
@@ -1,1 +1,21 @@
-#purrr practice
+# #purrr practice
+# checkpackages <- function(){
+#     if(!require("tidyverse")){
+#         install.packages("tidyverse")
+#         library("tidyverse")}
+#             library("tidyverse")}
+# checkpackages()
+
+
+library("gapminder")
+library("tidyverse")
+
+
+gapminder <- gapminder %>% mutate(year1950 = year - 1950)
+
+by_country <- gapminder %>%
+    group_by(continent, country)%>%
+    nest()
+
+head(by_country)
+by_country$data[[1]] #first element in the nested list 
